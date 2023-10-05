f6d006428fb834ace000402e1e5bbea1822d0ea8
YuriyMitrofanov <yurasell@yandex.ru>
2023-10-05 20:56:42 +0300

    progect is initialized

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import NavBar from "./components/navBar";
+
+function App() {
+    return (
+        <div className="container">
+            <NavBar/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/app/components/navBar/navBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <div className="container-fluid">
+                <ul className="nav">
+                    <li className="nav-item">First</li>
+                    <li className="nav-item">Second</li>
+                    <li className="nav-item">Third</li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
